275488790ff0d49b1d1c1b332229aebe51a8d96c
Boya Chiou <boya.chiou@gmail.com>
2019-02-23 21:22:22 +0800

    Create xxx.py

---
+++ xxx.py
@@ -0,0 +1,24 @@
+I. 
+What is an alpha?
+- Mathematical model: historical data > predictions of future
+- Alpha weight
+
+Alpha creation process?
+- idea(expression)->raw alpha(operations)->position(stats)->PnL(analyze)->Performance(IR,tvr,ret...)(revise)
+
+Equity market-neutral strategy
+- long/short balance position
+- risk handling
+
+Alpha category:
+- Intraday alphas
+- Daily alphas: Delay1(beginner), Delay 0
+- Weekly/monthly alphas
+
+Short position (sell without have none, borrow from broker to sell, and buy back to brocker) vs. long position
+long position: price goes up
+short position: price goes down
+
+II.
+Datasets: price-volume, fundamental, analyst estimate, relationship, sentiment, U.S. news data
+Mathematical Operators: time series vs. non time series (group neutralize, rank)
