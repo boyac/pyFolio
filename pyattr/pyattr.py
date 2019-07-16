7b40f9661c805a697869a7ad3177040cd71bd4af
Boya Chiou <boya.chiou@gmail.com>
2019-07-16 19:38:13 +0800

    Update pyattr.py

--- pyattr/pyattr.py
+++ pyattr/pyattr.py
@@ -4,6 +4,7 @@
 # @Last Modified by:   Boya Chiou
 # @Last Modified time: 2018-08-29 11:31:14
 
+
 class Performance(object):
 	"""docstring for Performance"""
 	def __init__(self, benchr=0, benchw=0, benchtotal=0, portr=0, portw=0):
@@ -16,19 +17,19 @@ class Performance(object):
 
 
 	def allocation_effect(self):
-		# positive contribution: OW in sector OP index, UW in sector UP index
+		# positive contribution: OW in sector outperform index, UW in sector underperform index
 		self.sector = (self.portw-self.benchw)*(self.benchr-self.benchtotal)
 		return self.sector
 
 
 	def selection_effect(self):
-		# positive contribution: OP benchmark sector
+		# positive contribution: portfolio outperform benchmark sector
 		self.selection = self.portw*(self.portr-self.benchr)
 		return self.selection
 
 
 	def interaction_effect(self):
-		# positive contribution: OW in port OP sector, UW in port UP sector
+		# positive contribution: OW in port outperform sector, UW in port underperform sector
 		self.interaction = (self.portw-self.benchw)*(self.portr-self.benchr)
 		return self.interaction
 
@@ -50,8 +51,8 @@ if __name__ == "__main__":
 	#Bechmark segment S&P500 Tech
 	#Benchmark return 9%
 	#Benchmark weight 7%
-	#Benchmark tech return 8%
-	#Benchmark tech weight 15%
+	#Benchmark/sub_sector tech return 8%
+	#Benchmark/sub_sector tech weight 15%
 	#Total benchmark return 7%
 	p = Performance(benchr=0.09, benchw=0.07, benchtotal=0.07, portr=0.08, portw=0.15)
 	"""
